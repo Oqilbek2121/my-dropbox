bebc1e44707e272b28a31817304fafa76a3f36b3
Oqilbek <100509683+Oqilbek2121@users.noreply.github.com>
2024-01-09 11:28:04 +0500

    Update Private.js

--- src/components/private/Private.js
+++ src/components/private/Private.js
@@ -1,16 +1,1 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
-import * as ROUTER from "../../Router/Routes";
-
-function PrivateRoute({ component: Component, ...rest }) {
-  const { currentUser } = useAuth();
-
-  if (currentUser) {
-    return <Route {...rest} render={props => <Component {...props} />} />;
-  } else {
-    return <Redirect to={ROUTER.ENTRY} />;
-  }
-}
-
-export default PrivateRoute;
+alert("no cheating");
