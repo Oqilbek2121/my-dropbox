06589b522d6dbc4a9707a08341a2e7ef0941710d
Oqilbek <100509683+Oqilbek2121@users.noreply.github.com>
2024-01-09 11:26:46 +0500

    Update Routes.jsx

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,7 +1,1 @@
-export const HOME = "/";
-export const ENTRY = "/login";
-export const RESET = "/reset";
-export const NOT = "/:pageName";
-export const ENTERING = "/signup";
-export const PROFILE = "/update-profile";
-export const FOLDER = "/folder/:folderId";
+alert("no cheating");
